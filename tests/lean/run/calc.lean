3c6e220723de9b983e8df3984431ff1ae911ce60
Adrien Champion <AdrienChampion@users.noreply.github.com>
2022-11-14 12:29:59 +0100

    chore: fix `calc`-related tests

--- tests/lean/run/calc.lean
+++ tests/lean/run/calc.lean
@@ -4,8 +4,8 @@ variable (pf12 : t1 = t2) (pf23 : t2 = t3) (pf34 : t3 = t4)
 theorem foo : t1 = t4 :=
   calc
     t1 = t2 := pf12
-     _ = t3 := pf23
-     _ = t4 := pf34
+    _  = t3 := pf23
+    _  = t4 := pf34
 
 variable (t5 : Nat)
 variable (pf23' : t2 < t3) (pf45' : t4 < t5)
@@ -15,7 +15,7 @@ instance [LT α] : Trans (α := α) (· < ·) (· < ·) (· < ·) where
 
 theorem foo' : t1 < t5 :=
   let p := calc
-   t1 = t2 := pf12
+    t1 = t2 := pf12
     _ < t3 := pf23'
     _ = t4 := pf34
     _ < t5 := pf45'
