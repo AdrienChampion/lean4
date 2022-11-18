6cee4ace660e83c21e7c8d75f3fd28513b25114f
Adrien Champion <AdrienChampion@users.noreply.github.com>
2022-11-18 08:59:23 +0100

    chore: add test combining calc and by

--- tests/lean/run/calc.lean
+++ tests/lean/run/calc.lean
@@ -13,7 +13,7 @@ variable (pf23' : t2 < t3) (pf45' : t4 < t5)
 instance [LT α] : Trans (α := α) (· < ·) (· < ·) (· < ·) where
   trans := sorry
 
-theorem foo' : t1 < t5 :=
+theorem foo₁ : t1 < t5 :=
   let p := calc
     t1 = t2 := pf12
     _  < t3 := pf23'
@@ -23,16 +23,30 @@ theorem foo' : t1 < t5 :=
   p
 
 -- same-line `calc <first relation>`
-theorem foo'' : t1 < t5 :=
+theorem foo₂ : t1 < t5 :=
   calc t1 = t2 := pf12
        _  < t3 := pf23'
        _  = t4 := pf34
        _  < t5 := pf45'
 
 -- `calc <first relation LHS>\n<indent><relation and relation RHS>`
-theorem foo''' : t1 < t5 :=
+theorem foo₃ : t1 < t5 :=
   calc t1
       = t2 := pf12
     _  < t3 := pf23'
     _  = t4 := pf34
     _  < t5 := pf45'
+
+-- `calc <first relation LHS>\n<indent><relation and relation RHS>`
+theorem foo₄ : t1 < t5 :=
+  calc t1 = t2 := pf12
+       _  < t3 := pf23'
+       _  = t4 := pf34
+       _  < t5 := pf45'
+
+theorem foo₅ : t1 = t4 :=
+  calc
+    t1 = t2 := pf12
+    _  = t3 := by
+      exact pf23
+    _  = t4 := pf34
